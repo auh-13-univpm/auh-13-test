d98c15977438128b8e9120cfa986e56fd73b9458
Paolo <polyp91@gmail.com>
2013-05-22 20:24:42 +0200

    creata view base di esempio per OSM

--- osmcsv/src/it/univpm/auh/osmcsv/MainActivity.java
+++ osmcsv/src/it/univpm/auh/osmcsv/MainActivity.java
@@ -1,15 +1,27 @@
 package it.univpm.auh.osmcsv;
 
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	MapView mapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        
+        mapView = new MapView(this, 256);
+        mapView.setClickable(true);
+        mapView.setBuiltInZoomControls(true);
+ 
+        mapView.getController().setZoom(10);
+        mapView.getController().setCenter(new GeoPoint(39.461078, 2.856445));
+ 
+        setContentView(mapView);
     }
 
 
